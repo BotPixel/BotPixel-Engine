acd1f217504abdda5cbc194287f466c443a09d6a
RXPlays <46143060+RXPlays@users.noreply.github.com>
2019-03-17 12:06:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var #1 = io.of('/1');
+
+id1.on('connection', function(socket){
+  socket.on('player move', function(msg){
+    id1.emit('player move', msg);
+    socket.on('chat message', function(msg){
+    id1.emit('chat message', msg);
+    socket.on('shoot', function(msg){
+    id1.emit('shoot', msg);
+    socket.on('join', function(msg){
+    id1.emit('join', msg);
+    socket.on('killfeed', function(msg){
+    id1.emit('killfeed', msg);
+    socket.on('hurt', function(msg){
+    id1.emit('hurt', msg);
+    socket.on('death', function(msg){
+    id1.emit('death', msg);
+    });
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');

74a4c415cc739be51631a9991be4e525542b637a
RXPlays <46143060+RXPlays@users.noreply.github.com>
2019-03-17 11:12:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ io.on('connection', function(socket){
   });
 });
 
+io.on('connection', function(socket){
+  socket.on('player move', function(msg){
+    io.emit('player move', msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('shoot', function(msg){
+    io.emit('shoot', msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('join', function(msg){
+    io.emit('join', msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('leave', function(msg){
+    io.emit('leave', msg);
+  });
+});
+
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
